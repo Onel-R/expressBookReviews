45e85b894ceaaa039a73a126669459e03513eb21
Onel-R <leonreuben@yahoo.com>
2025-06-10 14:59:25 -0400

    Asyn_Await tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,21 +25,21 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  res.send(JSON.stringify({books}, null, 4))
+public_users.get('/', async function (req, res) {
+  res.send(JSON.stringify(await {books}, null, 4))
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
-  res.send(books[isbn]);
+  res.send(await books[isbn]);
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
     const author = req.params.author;
     let filteredBooks = [];
-    Object.values(books).forEach(value => {
+    await Object.values(books).forEach(value => {
         if(value.author === author) {
             filteredBooks.push(value);
         }
@@ -49,10 +49,10 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
     const title = req.params.title;
     let filteredBooks = [];
-    Object.values(books).forEach(value => {
+    await Object.values(books).forEach(value => {
         if(value.title === title) {
             filteredBooks.push(value);
         }
